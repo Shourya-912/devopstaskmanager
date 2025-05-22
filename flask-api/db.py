7e44fd964e74b0d5dd9ed0deba15937d52997987
Shourya Chourasia <138221216+Shourya-912@users.noreply.github.com>
2025-05-22 17:32:15 +0530

    Update db.py

--- flask-api/db.py
+++ flask-api/db.py
@@ -3,8 +3,8 @@ import time
 from config import DB_CONFIG
 
 def get_connection():
-    retries = 5
-    while retries>0:
+    retries = 10
+    for i in range(retries):
         try:
             return mysql.connector.connect(
                 host = DB_CONFIG['host'],
@@ -13,7 +13,7 @@ def get_connection():
                 database = DB_CONFIG['database']
             ) 
         except mysql.connector.Error as err:
-            print("MySQL is not ready retrying in 5 seconds")
+            print("MySQL is not ready (attempt {i+1}/{retries}) -retrying in 5 seconds")
             time.sleep(5)
             retries-=1
     raise Exception("Failed to connect to MySQL after several retries")
