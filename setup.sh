b5cc380f925004d3fe3fb9dd53286d2d15cf72f6
Shourya Chourasia <138221216+Shourya-912@users.noreply.github.com>
2025-05-22 19:52:14 +0530

    Create setup.sh

---
+++ setup.sh
@@ -0,0 +1,37 @@
+#!/bin/bash
+ 
+echo "==== Updating packages ===="
+sudo dnf update -y
+ 
+echo "==== Installing Docker ===="
+sudo dnf install -y docker
+ 
+echo "==== Starting Docker ===="
+sudo systemctl start docker
+sudo systemctl enable docker
+docker --version
+
+echo "==== Adding user to Docker group ===="
+sudo usermod -aG docker ec2-user
+ 
+echo "==== Installing Docker Compose ===="
+sudo curl -L "https://github.com/docker/compose/releases/download/v2.24.4/docker-compose-linux-x86_64" -o /usr/local/bin/docker-compose
+sudo chmod +x /usr/local/bin/docker-compose
+docker-compose --version
+ 
+echo "==== Installing Git ===="
+sudo dnf install -y git
+git --version
+ 
+echo "==== Cloning your repo ===="
+git clone https://github.com/Shourya-912/devopstaskmanager.git
+ 
+echo "==== Running Docker Compose ===="
+cd devopstask manager
+
+echo "=======git pull if changes done========"
+git pull
+docker-compose down -v
+docker image prune -a -f
+docker-compose up --build
+
