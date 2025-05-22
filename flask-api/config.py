a9fec480453600371d1ea1f0602b3f982fd70065
Shourya Chourasia <138221216+Shourya-912@users.noreply.github.com>
2025-05-22 14:45:54 +0530

    Update config.py

--- flask-api/config.py
+++ flask-api/config.py
@@ -6,7 +6,7 @@ from dotenv import load_dotenv
 load_dotenv()  # Load .env variables
  
 DB_CONFIG = {
-    "host": "mysql",
+    "host": os.getenv("MYSQL_HOST"),
     "database": os.getenv("MYSQL_NAME"),
     "user": os.getenv("MYSQL_USER"),
     "password": os.getenv("MYSQL_PASSWORD")
